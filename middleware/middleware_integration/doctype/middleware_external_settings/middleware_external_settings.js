2cfbf91c1b31437327d67e71a6dc52e4c0ab04ca
tinumaria <tinumariadavis@gmail.com>
2026-07-03 14:55:57 +0530

    feat(Middleware External Settings): add new doctype

---
+++ middleware/middleware_integration/doctype/middleware_external_settings/middleware_external_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Aravind R and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Middleware External Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
