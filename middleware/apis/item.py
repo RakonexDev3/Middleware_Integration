ef26504b11bed9b99eb6eb23ec18be6e5925c664
Aravind R <aravindrajeshchry@gmail.com>
2026-09-18 13:10:35 +0530

    feat(Item): add installation item details API

---
+++ middleware/apis/item.py
@@ -0,0 +1,26 @@
+import frappe
+
+@frappe.whitelist()
+def installation_item_details(item_code=None):
+    if not item_code:
+        item_code = frappe.local.form_dict
+
+    item_code = frappe.parse_json(item_code)
+
+    item = frappe.get_value("Item", item_code, ["item_code", "item_name", "stock_uom", "installation_type", "installation_level"], as_dict=True)
+
+    level_time = frappe.get_value("Installation Level", item.installation_level, "estimated_time") if item.installation_level else None
+
+    teams = frappe.get_list("Installation Team", filters={"team_level": item.installation_level})
+
+    return {
+        "item_details": {
+            "item_code": item.item_code,
+            "item_name": item.item_name,
+            "stock_uom": item.stock_uom,
+            "installation_type": True if item.installation_type == 1 else False,
+            "installation_level": item.installation_level,
+            "estimated_installation_time": level_time,
+        },        
+        "teams": [team.name for team in teams]
+    }
