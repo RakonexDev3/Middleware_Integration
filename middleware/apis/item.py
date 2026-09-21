38957294381f1095308e7a6344d314427a8b01d1
tinumaria <tinumariadavis@gmail.com>
2026-09-21 15:42:01 +0530

    chore(API): add delivery zone filtering

--- middleware/apis/item.py
+++ middleware/apis/item.py
@@ -1,7 +1,8 @@
 import frappe
 
+
 @frappe.whitelist()
-def installation_item_details(item_code=None):
+def installation_item_details(item_code=None, delivery_zone=None):
     if not item_code:
         return {"error": "Item code is required."}
     
@@ -9,7 +10,32 @@ def installation_item_details(item_code=None):
 
     level_time = frappe.get_value("Installation Level", item.installation_level, "estimated_time") if item.installation_level else None
 
-    teams = frappe.get_list("Installation Team", filters={"team_level": item.installation_level})
+    team_names = frappe.get_list("Installation Team", filters={"team_level": item.installation_level}, pluck="name")
+
+    if delivery_zone:
+        zone_teams = frappe.get_all(
+            "Delivery Zone Team",
+            filters={
+                "parent": delivery_zone,
+                "parenttype": "Delivery Zone",
+            },
+            pluck="installation_team",
+        )
+
+        team_names = [team for team in team_names if team in zone_teams]
+        zones = [delivery_zone]
+
+    else:
+        zone_records = frappe.get_all(
+            "Delivery Zone Team",
+            filters={
+                "installation_team": ["in", team_names],
+                "parenttype": "Delivery Zone",
+            },
+            pluck="parent",
+        )
+
+        zones = list(dict.fromkeys(zone_records))
 
     return {
         "item_details": {
@@ -20,5 +46,6 @@ def installation_item_details(item_code=None):
             "installation_level": item.installation_level,
             "estimated_installation_time": level_time,
         },        
-        "teams": [team.name for team in teams]
+        "teams": team_names,
+        "zones": zones,
     }
